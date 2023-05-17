bcdcfe98a269f1090bd72dfb6e68b9f837a5b7c6
Nicolas Tsokas <nikolaos.tsokas@swisscom.com>
2023-05-17 15:04:02 +0200

    Minor pmacctd.conf file building editing

--- library/py/setup_tools.py
+++ library/py/setup_tools.py
@@ -12,12 +12,12 @@ class KModuleParams:
         self.test_folder = os.path.dirname(filename)
         self.test_name = os.path.basename(self.test_folder)
         self.test_mount_folder = self.test_folder + '/pmacct_mount'
+        self.results_output_folder = self.results_mount_folder + '/pmacct_output'
         self.pmacct_mount_folder = '/var/log/pmacct'
         self.test_conf_file = self.test_folder + '/pmacctd.conf'
         self.results_folder = os.getcwd() + '/results/' + self.test_name
         self.results_conf_file = self.results_folder + '/pmacctd.conf'
         self.results_mount_folder = self.results_folder + '/pmacct_mount'
-        self.results_output_folder = self.results_mount_folder + '/pmacct_output'
         self.kafka_topic_name = 'test.topic.' + secrets.token_hex(4)[:8]
         self.results_log_file = self.results_output_folder + '/pmacctd.log'
 
@@ -44,23 +44,30 @@ def prepare_test_env(_module):
     os.umask(_mask)
     logger.debug('Folders created')
 
-    # Edit configuration
-    config.replace_value_of_key('logfile', '/var/log/pmacct/pmacct_output/pmacctd.log')
-    config.replace_value_of_key('pidfile', '/var/log/pmacct/pmacct_output/pmacctd.pid')
+    # Preparing actual pmacctd.conf file
+
+    # Files in mounted folder, for pmacct to read
+    config.replace_value_of_key('kafka_config_file', params.pmacct_mount_folder + '/librdkafka.conf')
+    config.replace_value_of_key('pre_tag_map', params.pmacct_mount_folder + '/pretag.map')
+    config.replace_value_of_key('flow_to_rd_map', params.pmacct_mount_folder + '/f2rd-00.map')
+    config.replace_value_of_key('aggregate_primitives', params.pmacct_mount_folder + '/custom-primitives-00.lst')
+
+    # Files in output folder, for pmacct to write
+    config.replace_value_of_key('logfile', params.results_output_folder + '/pmacctd.log')
+    config.replace_value_of_key('pidfile', params.results_output_folder + '/pmacctd.pid')
+    config.replace_value_of_key('avro_schema_output_file', params.results_output_folder + '/flow_avroschema.avsc')
+
+    # Replace specific operational values
     config.replace_value_of_key('kafka_topic', params.kafka_topic_name)
-    config.replace_value_of_key('kafka_config_file', '/var/log/pmacct/librdkafka.conf')
     config.replace_value_of_key('kafka_avro_schema_registry', 'http://schema-registry:8081')
-    # Later pmacct versions expect avro_schema_output_file instead of avro_schema_file
-    config.replace_value_of_key('avro_schema_file', '/var/log/pmacct/pmacct_output/flow_avroschema.avsc')
     config.replace_value_of_key('debug', 'true')
-    config.replace_value_of_key('pre_tag_map', '/var/log/pmacct/pretag.map')
-    config.replace_value_of_key('flow_to_rd_map', '/var/log/pmacct/f2rd-00.map')
     config.replace_value_of_key('nfacctd_ip', '0.0.0.0')
     config.replace_value_of_key('nfacctd_port', '8989')
-    config.replace_value_of_key('aggregate_primitives', '/var/log/pmacct/custom-primitives-00.lst')
+
+    # Output to new conf file in mount folder
     config.print_to_file(params.results_conf_file)
 
-    # Copy existing files in pmacct_mount to result mount folder
+    # Copy existing files in pmacct_mount to result (=actual) mounted folder
     if os.path.exists(params.test_mount_folder):
         src_files = os.listdir(params.test_mount_folder)
         count = 0
