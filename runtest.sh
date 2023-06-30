330815cc6bf9e7348c254edd293ff158da25edfa
Nicolas Tsokas <nikolaos.tsokas@swisscom.com>
2023-06-30 22:29:53 +0200

    Added runtest for supporting multiple tests in a single session

---
+++ runtest.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# exit if there is no argument
+if [ -z "$1" ]; then
+    echo "No argument supplied"
+    exit 1
+fi
+
+cd "$(dirname "$0")"
+
+for arg in "$@"; do
+  files=( tests/${arg}* )
+  # Check the length of the array
+  if [ ${#files[@]} -gt 1 ]; then
+      echo "Wildcard pattern tests/${arg}* matched more than one entry"
+      exit 1
+  fi
+done
+
+if [ "$#" -gt 1 ]; then
+  args=""
+  for arg in "$@"; do
+    args="$args tests/${arg}*"
+  done
+  python -m pytest $args --log-cli-level=DEBUG --html=results/report.html
+else
+  python -m pytest tests/${1}* --log-cli-level=DEBUG --html=results/report${1}.html
+  testdir=${files[0]}
+  echo "Moving report to the test case specific folder"
+  mv results/report${1}.html ${testdir/tests/results}/
+  mv results/assets ${testdir/tests/results}/
+fi
