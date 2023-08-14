b9d9aa782c0e6982dface76b0e32d1444b80dd66
Nicolas Tsokas <nikolaos.tsokas@swisscom.com>
2023-08-14 21:02:24 +0200

    Setup dind fix for sh environment

--- tools/setup_dind.sh
+++ tools/setup_dind.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "$0" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR/..
@@ -17,7 +17,11 @@ echo "Installing python library requirements"
 pip install -r requirements.txt || exit $?
 
 echo "Pulling docker images from repository"
-cat settings.conf | grep -Po "(?<==).+" | xargs docker pull
+cat settings.conf | awk -F '=' '{print $2}' | while read -r value; do
+  if [ ! -z $value ]; then
+    docker pull "$value";
+  fi;
+done
 
 echo "Building traffic reproducer docker images"
 cd tools/pcap_player
