1c3a9049813046872c792b7ea679a7dc77c30634
Nicolas Tsokas <nikolaos.tsokas@swisscom.com>
2023-08-14 22:07:33 +0200

    Changed xargs to while, to work in dind apline

--- library/sh/traffic_docker/stop_all.sh
+++ library/sh/traffic_docker/stop_all.sh
@@ -3,4 +3,4 @@
 # Forcefully deleting all traffic containers
 
 echo "Removing all traffic containers"
-docker ps --all --filter name="traffic-reproducer-" --quiet | xargs docker rm --force
+docker ps --all --filter name="traffic-reproducer-" --quiet | while read -r value; do docker rm --force "$value"; done 
