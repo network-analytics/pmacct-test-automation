eaa26acd799319bbd20c16de8fcd8359a75f4d8e
Nicolas Tsokas <nikolaos.tsokas@swisscom.com>
2023-12-16 11:07:10 +0100

    Changed traffic-reproducer images tags to local for consistency. Added pytest to test case 300 imports.

--- tools/pcap_player/build_docker_images.sh
+++ tools/pcap_player/build_docker_images.sh
@@ -5,5 +5,5 @@ IMG=debian
 SCRIPT_DIR=$( cd -- "$( dirname -- "$0" )" &> /dev/null && pwd )
 
 echo "Building traffic reproducer docker images"
-docker build -t traffic-reproducer -f $SCRIPT_DIR/single/Dockerfile_$IMG $SCRIPT_DIR || exit $?
-docker build -t traffic-reproducer-multi -f $SCRIPT_DIR/multi/Dockerfile_$IMG $SCRIPT_DIR || exit $?
+docker build -t traffic-reproducer:local -f $SCRIPT_DIR/single/Dockerfile_$IMG $SCRIPT_DIR || exit $?
+docker build -t traffic-reproducer-multi:local -f $SCRIPT_DIR/multi/Dockerfile_$IMG $SCRIPT_DIR || exit $?
