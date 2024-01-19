710c0034e8e8b10ccb4572dd824f25c42892ba5c
Leonardo Rodoni <Leonardo.Rodoni@swisscom.com>
2024-01-19 12:47:05 +0100

    update and improve test cases - add bmp peerdown test from 6wind/FRR

---
+++ tests/205-BMP-6wind-FRR-peer_down/205_test.py
@@ -0,0 +1,24 @@
+
+from library.py.setup_tools import KModuleParams
+import library.py.scripts as scripts
+import library.py.helpers as helpers
+import logging, pytest, sys
+import library.py.test_tools as test_tools
+logger = logging.getLogger(__name__)
+
+testParams = KModuleParams(sys.modules[__name__], daemon='nfacctd', ipv4_subnet='192.168.100.')
+
+@pytest.mark.nfacctd
+@pytest.mark.bmp
+@pytest.mark.bmp_only
+@pytest.mark.bmpv3
+def test(test_core_no_kafka, consumer_setup_teardown):
+    main(consumer_setup_teardown[0])
+
+def main(consumer):
+    assert scripts.replay_pcap_detached(testParams.pcap_folders[0], 0)
+
+    assert test_tools.read_and_compare_messages(consumer, testParams, 'bmp-00',
+        ['timestamp', 'bmp_router_port', 'timestamp_arrival'])
+
+    assert not helpers.check_regex_sequence_in_file(testParams.pmacct_log_file, ['ERROR|WARN(?!.*Unable to get kafka_host)'])
