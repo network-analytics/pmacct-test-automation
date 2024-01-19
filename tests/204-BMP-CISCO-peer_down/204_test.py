5b2aa9a47be78f182ca988daf38b83773e302cb0
Leonardo Rodoni <Leonardo.Rodoni@swisscom.com>
2024-01-19 12:01:25 +0100

    update and improve test cases - bmp PeerDown test

---
+++ tests/204-BMP-CISCO-peer_down/204_test.py
@@ -0,0 +1,23 @@
+from library.py.setup_tools import KModuleParams
+import library.py.scripts as scripts
+import library.py.helpers as helpers
+import logging, pytest, sys
+import library.py.test_tools as test_tools
+logger = logging.getLogger(__name__)
+
+testParams = KModuleParams(sys.modules[__name__], daemon='nfacctd')
+
+@pytest.mark.nfacctd
+@pytest.mark.bmp
+@pytest.mark.bmp_only
+@pytest.mark.bmpv3
+def test(test_core, consumer_setup_teardown):
+    main(consumer_setup_teardown[0])
+
+def main(consumer):
+    assert scripts.replay_pcap_detached(testParams.pcap_folders[0], 0)
+
+    assert test_tools.read_and_compare_messages(consumer, testParams, 'bmp-00',
+        ['seq', 'timestamp', 'timestamp_arrival', 'bmp_router_port'])
+
+    assert not helpers.check_regex_sequence_in_file(testParams.pmacct_log_file, ['ERROR|WARN'])
