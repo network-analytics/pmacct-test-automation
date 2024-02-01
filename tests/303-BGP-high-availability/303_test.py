ee48dd5aa58af7b70a26ce27016a54de9492d92e
Nicolas Tsokas <nikolaos.tsokas@swisscom.com>
2024-02-01 15:04:40 +0100

    Fixed timing issues in 303, which caused inconsistent outcome under certain conditions

--- tests/303-BGP-high-availability/303_test.py
+++ tests/303-BGP-high-availability/303_test.py
@@ -2,7 +2,7 @@
 from library.py.test_params import KModuleParams
 import library.py.scripts as scripts
 import library.py.helpers as helpers
-import logging, pytest, sys, time
+import logging, pytest, sys, time, datetime
 import library.py.test_tools as test_tools
 logger = logging.getLogger(__name__)
 
@@ -16,7 +16,21 @@ def test(test_core_redis, consumer_setup_teardown):
     main(consumer_setup_teardown)
 
 def main(consumers):
-    #time.sleep(3)
+    curr_sec = datetime.datetime.now().second
+    logger.info('Minute seconds: ' + str(curr_sec))
+
+    # Some additional timing constraints for this test
+    # Make sure traffic reproducer 03 does not send packets before the others
+    if curr_sec < 5:
+        wait_sec = 5 - curr_sec
+        logger.debug('Waiting ' + str(wait_sec) + ' seconds')
+        time.sleep(wait_sec)
+    # Make sure that traffic reproducers do not start in different minutes
+    elif curr_sec > 55:
+        wait_sec = 65 - curr_sec
+        logger.debug('Waiting ' + str(wait_sec) + ' seconds')
+        time.sleep(wait_sec)
+    #time.sleep(25)
     #assert scripts.send_signal_to_pmacct(testParams.pmacct[0].name, 'SIGRTMIN')
 
 
@@ -25,9 +39,9 @@ def main(consumers):
     assert pcap_folder_multi
     assert scripts.replay_pcap_detached(pcap_folder_multi)
 
-    assert test_tools.read_messages_dump_only(consumers.getReaderOfTopicStartingWith('daisy.bgp'), testParams, 20)
+    assert test_tools.read_messages_dump_only(consumers.getReaderOfTopicStartingWith('daisy.bgp'), testParams, 60)
     assert scripts.send_signal_to_pmacct(testParams.pmacct[0].name, 'SIGRTMIN')
-    time.sleep(5)
+    #time.sleep(5)
     assert test_tools.read_messages_dump_only(consumers.getReaderOfTopicStartingWith('daisy.bgp'), testParams, 20)
 
 
