b3a0d389eaba86c0ef2372988d90b07ce8ac9ff3
Nicolas Tsokas <nikolaos.tsokas@swisscom.com>
2024-02-01 15:24:05 +0100

    Uncluttering 303

--- tests/303-BGP-high-availability/303_test.py
+++ tests/303-BGP-high-availability/303_test.py
@@ -19,8 +19,7 @@ def main(consumers):
     curr_sec = datetime.datetime.now().second
     logger.info('Minute seconds: ' + str(curr_sec))
 
-    # Some additional timing constraints for this test
-    # Make sure traffic reproducer 03 does not send packets before the others
+    # Timing constraints
     if curr_sec < 5:
         wait_sec = 5 - curr_sec
         logger.debug('Waiting ' + str(wait_sec) + ' seconds')
@@ -30,32 +29,21 @@ def main(consumers):
         wait_sec = 65 - curr_sec
         logger.debug('Waiting ' + str(wait_sec) + ' seconds')
         time.sleep(wait_sec)
-    #time.sleep(25)
-    #assert scripts.send_signal_to_pmacct(testParams.pmacct[0].name, 'SIGRTMIN')
 
-
+    # Prepare the multicast pcap player (mount points, traffic-repro.yml, docker-compose.yml, etc.)
     pcap_folder_multi = test_tools.prepare_multicast_pcap_player(testParams.results_folder, testParams.pcap_folders[0],
         testParams.pmacct, 0, testParams.fw_config)
     assert pcap_folder_multi
+    # Play traffic against all 3 nfacctd instances
     assert scripts.replay_pcap_detached(pcap_folder_multi)
-
+    # Read messages for one minute --- messages should refer to first nfacctd instance "locA"
     assert test_tools.read_messages_dump_only(consumers.getReaderOfTopicStartingWith('daisy.bgp'), testParams, 60)
+    # Reset timer of nfacctd-00
     assert scripts.send_signal_to_pmacct(testParams.pmacct[0].name, 'SIGRTMIN')
-    #time.sleep(5)
+    # Read messages for a while --- message should now refer to second nfacctd instance "locB"
     assert test_tools.read_messages_dump_only(consumers.getReaderOfTopicStartingWith('daisy.bgp'), testParams, 20)
 
-
     scripts.stop_and_remove_traffic_container(testParams.pcap_folders[0])
-
-
-
-
-    # assert scripts.send_signal_to_pmacct(testParams.pmacct[0].name, 'SIGRTMIN')
-    #
-    # assert scripts.replay_pcap_detached(pcap_folder_multi)
-    # assert test_tools.read_messages_dump_only(consumers.getReaderOfTopicStartingWith('daisy.bgp'),
-    #     testParams, 'bgp-00', ['seq', 'timestamp', 'peer_tcp_port', 'writer_id'])
-
 
 
 
