5b75455afab4258063dc06bfe3483a1aaf368347
Nicolas Tsokas <nikolaos.tsokas@swisscom.com>
2024-02-07 08:05:16 +0100

    Line-based log comparison; conf back to debug:false

--- tests/303-BGP-high-availability/303_test.py
+++ tests/303-BGP-high-availability/303_test.py
@@ -1,7 +1,8 @@
 
 from library.py.test_params import KModuleParams
+import library.py.helpers as helpers
 import library.py.scripts as scripts
-import logging, pytest
+import logging, pytest, time
 import library.py.test_tools as test_tools
 logger = logging.getLogger(__name__)
 
@@ -16,6 +17,39 @@ def test(test_core_redis, consumer_setup_teardown):
 
 def main(consumers):
     test_tools.avoid_time_period_in_seconds(5, 10)
+    repro_ip = helpers.get_repro_ip_from_pcap_folder(testParams.pcap_folders[0])
+
+    logfile = testParams.log_files.getFileLike('log-00')
+    test_tools.transform_log_file(logfile, repro_ip)
+    with open(logfile, 'r') as f:
+        loglines = f.read().split('\n')
+
+    assert helpers.check_regex_sequence_in_file(testParams.pmacct[0].pmacct_log_file, [loglines[0], loglines[1]])
+    assert helpers.check_regex_sequence_in_file(testParams.pmacct[1].pmacct_log_file, [loglines[0], loglines[2]])
+    assert helpers.check_regex_sequence_in_file(testParams.pmacct[2].pmacct_log_file, [loglines[0], loglines[2]])
+
+    pcap_folder_multi = test_tools.prepare_multicollector_pcap_player(testParams.results_folder,
+        testParams.pcap_folders[0], testParams.pmacct, testParams.fw_config)
+    assert pcap_folder_multi
+    # Play traffic against all 3 nfacctd instances
+    assert scripts.replay_pcap_detached(pcap_folder_multi)
+    # Read messages for one minute --- messages should refer to first nfacctd instance "locA"
+    assert test_tools.read_messages_dump_only(consumers.getReaderOfTopicStartingWith('daisy.bgp'), testParams, 60)
+
+    time.sleep(5)
+
+    assert helpers.check_regex_sequence_in_file(testParams.pmacct[0].pmacct_log_file, [loglines[3]])
+    assert helpers.check_regex_sequence_in_file(testParams.pmacct[1].pmacct_log_file, [loglines[3]])
+    assert helpers.check_regex_sequence_in_file(testParams.pmacct[2].pmacct_log_file, [loglines[3]])
+
+    assert scripts.send_signal_to_pmacct(testParams.pmacct[0].name, 'SIGRTMIN')
+
+    time.sleep(2)
+
+    assert helpers.check_regex_sequence_in_file(testParams.pmacct[0].pmacct_log_file, [loglines[4], loglines[2]])
+    assert helpers.check_regex_sequence_in_file(testParams.pmacct[1].pmacct_log_file, [loglines[1]])
+
+    return True
 
     # Prepare the multicast pcap player (mount points, traffic-repro.yml, docker-compose.yml, etc.)
     pcap_folder_multi = test_tools.prepare_multicollector_pcap_player(testParams.results_folder, testParams.pcap_folders[0],
