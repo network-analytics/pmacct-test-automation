d60c430960b2aeecd4f23379549ba8f707d95b34
Nicolas Tsokas <nikolaos.tsokas@swisscom.com>
2024-02-07 08:33:24 +0100

    Fixed comment in resource monitoring script

--- tools/monitor.sh
+++ tools/monitor.sh
@@ -19,19 +19,7 @@ rm -f ${OUTPUT_FILE}
 echo "Monitoring pmacct containers resource consumption -- outputting to file: $OUTPUT_FILE"
 
 # No concurrency issues with test_name_logging fixture are expected, even though logging to the same file,
-# because that fixture only logs before pmacct is deployed (since its scope is module)
-#while true; do
-#  if docker inspect pmacct >/dev/null 2>&1; then
-#    dte=$( date )
-#    msg=$( docker stats pmacct --no-stream --format '{{ json . }}' 2>/dev/null )
-#    if [ $? -eq 0 ]; then
-#      echo "$dte $msg" >> ${OUTPUT_FILE}
-#    fi
-#  fi
-#  sleep 1
-#done
-
-
+# because that fixture runs before any pmacct instances are deployed and after these are stopped
 while true; do
   docker ps -af "name=(nfacctd|pmbmpd|pmbgpd)-\d+" --format '{{.Names}}' | while read -r value; do
     if docker inspect $value >/dev/null 2>&1; then
