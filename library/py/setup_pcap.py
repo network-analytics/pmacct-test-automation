6320e8410838a2dab5d2978ccb59284086311fbc
Nicolas Tsokas <nikolaos.tsokas@swisscom.com>
2024-02-19 08:31:41 +0100

    Added traffic-repro.yml to container-setup, added new pcap creation functionality (draft)

--- library/py/setup_pcap.py
+++ library/py/setup_pcap.py
@@ -83,14 +83,79 @@ def prepare_pcap(params: KModuleParams):
     for i in range(len(test_config_files)):
         prepare_pcap_folder(params, i, test_config_files[i], test_pcap_files[i])
 
+    def get_REPRO_IP_and_BGP_ID(traffic_config_file: str):
+        with open(traffic_config_file) as f:
+            data = yaml.load(f, Loader=yaml.FullLoader)
+        repro_info = [data['network']['map'][0]['repro_ip'], None]
+        if 'bgp_id' in data['network']['map'][0]:
+            repro_info[1] = data['network']['map'][0]['bgp_id']
+        return repro_info
+
     with open(params.test_folder + '/container-setup.yml') as f:
         data = yaml.load(f, Loader=yaml.FullLoader)
     for container in data['containers']:
         folder_name = params.results_folder + '/' + container['name']
         os.makedirs(folder_name)
         params.traffic_folders.append(folder_name)
-        for process in container['processes']:
-            pcap_file = params.test_folder + '/' + process['pcap']
 
-        shutil.copy(test_config_file, results_pcap_folder + '/pcap0/traffic-reproducer.yml')
-        shutil.copy(test_pcap_file, results_pcap_folder + '/pcap0/traffic.pcap')
+        # Make sure traffic-reproducer.yml files of all pcap folders refer to the same IP and BGP_ID
+        # Otherwise, it is not possible for a single server (container) to replay these traffic data
+        config_file_src = params.test_folder + '/' + container['processes'][0]['config']
+        repro_info = get_REPRO_IP_and_BGP_ID(config_file_src)
+        for i in range(len(container['processes'])):
+            config_file_src = params.test_folder + '/' + container['processes'][i]['config']
+            if repro_info != get_REPRO_IP_and_BGP_ID(config_file_src):
+                logger.error('IP and/or BGP_ID for the same traffic reproducer do not match!')
+                return None
+        repro_ip = repro_info[0]
+        isIPv6 = ':' in repro_info[0]
+
+        with open(params.root_folder + '/library/sh/traffic_docker/docker-compose-template.yml') as f:
+            data_dc = yaml.load(f, Loader=yaml.FullLoader)
+
+        data_dc['services']['traffic-reproducer']['container_name'] = container['name']
+        data_dc['services']['traffic-reproducer']['image'] = params.fw_config.get('TRAFFIC_REPRO_IMG')
+        data_dc['services']['traffic-reproducer']['volumes'][0] = folder_name + ':/pcap'
+
+        if isIPv6:
+            data_dc['services']['traffic-reproducer']['networks']['pmacct_test_network']['ipv6_address'] = \
+                repro_ip if len(params.test_subnet_ipv6) < 1 else repro_ip.replace(params.test_subnet_ipv6, 'fd25::10')
+            del data_dc['services']['traffic-reproducer']['networks']['pmacct_test_network']['ipv4_address']
+        else:
+            data_dc['services']['traffic-reproducer']['networks']['pmacct_test_network']['ipv4_address'] = \
+                repro_ip if len(params.test_subnet_ipv4) < 1 else repro_ip.replace(params.test_subnet_ipv4, '172.21.1.10')
+            del data_dc['services']['traffic-reproducer']['networks']['pmacct_test_network']['ipv6_address']
+
+        with open(folder_name + '/docker-compose.yml', 'w') as f:
+            yaml.dump(data_dc, f, default_flow_style=False, sort_keys=False)
+
+        for i in range(len(container['processes'])):
+            process = container['processes'][i]
+            pcap_file_src = params.test_folder + '/' + process['pcap']
+            pcap_file_dst = folder_name + '/' + process['pcap']
+            if not os.path.isfile(pcap_file_dst):
+                shutil.copy(pcap_file_src, pcap_file_dst)
+            os.makedirs(folder_name + '/pcap' + str(i))
+            config_file_src = params.test_folder + '/' + process['config']
+            config_file_dst = folder_name + '/pcap' + str(i) + '/traffic-reproducer.yml'
+            shutil.copy(config_file_src, config_file_dst)
+            with open(config_file_dst) as f:
+                data = yaml.load(f, Loader=yaml.FullLoader)
+            data['pcap'] = '/pcap/' + process['pcap']
+
+            # adding pmacct IP address
+            # isIPv6 = ':' in data['network']['map'][0]['repro_ip']
+            pmacct = params.get_pmacct_with_name(process['collector'])
+            pmacct_ip = pmacct.ipv6 if isIPv6 else pmacct.ipv4
+            logger.debug('Traffic uses ' + ('IPv6' if isIPv6 else 'IPv4'))
+            for k in ['bmp', 'bgp', 'ipfix']:
+                if k in data:
+                    data[k]['collector']['ip'] = pmacct_ip
+
+            if isIPv6:
+                fix_repro_ip_in_config(params.test_subnet_ipv6, data, 'fd25::10')
+            else:
+                fix_repro_ip_in_config(params.test_subnet_ipv4, data, '172.21.1.10')
+
+            with open(folder_name + '/pcap' + str(i) + '/traffic-reproducer.yml', 'w') as f:
+                yaml.dump(data, f, default_flow_style=False, sort_keys=False)
