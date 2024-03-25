3ceabc5266fea874645969c91f923ed6cf9899bc
Nicolas Tsokas <nikolaos.tsokas@swisscom.com>
2024-03-25 17:40:45 +0100

    Wrapper introduced to stop scripts, too

--- library/sh/traffic_docker/stop_docker_compose.sh
+++ library/sh/traffic_docker/stop_docker_compose.sh
@@ -5,5 +5,9 @@ if [ -z "$1" ]; then
     echo "No docker-compose file supplied"
     exit 1
 fi
+
+# find directory, where this script resides
+SCRIPT_DIR=$( cd -- "$( dirname -- "$0" )" &> /dev/null && pwd )
+
 DOCKER_COMPOSE_FILE="$1"
-docker-compose -f $DOCKER_COMPOSE_FILE down
+$SCRIPT_DIR/../../../tools/docker_compose_wrapper.sh -f $DOCKER_COMPOSE_FILE down
